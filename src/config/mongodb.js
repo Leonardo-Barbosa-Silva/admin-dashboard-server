8a9bca7915f0d1c633716ff4d20d022d514a6037
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-08-09 17:26:40 -0300

    initial server setup

---
+++ src/config/mongodb.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+
+dotenv.config()
+
+const USERNAME = process.env.MONGO_USERNAME
+const PASSWORD = encodeURIComponent(process.env.MONGO_PASSWORD)
+
+
+const connectDB = async (server) => {
+    mongoose.connect(
+        `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.pb3badg.mongodb.net/app`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        )
+            .then( async (conn) => {
+                await server();
+                console.log(`Database is running on: ${conn.connection.host}`.gray)
+            })
+            .catch( (error) => {
+                console.log(`${error}`.red)
+                process.exit(1)
+            })
+}
+
+
+export default connectDB;
---
+++ src/router/router.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import clientRoutes from './clients/index.js';
+import generalRoutes from './general/index.js';
+import salesRoutes from './sales/index.js';
+import managementRoutes from './management/index.js';
+
+const router = express.Router();
+
+
+router.use('/clients', clientRoutes);
+router.use('/general', generalRoutes);
+router.use('/sales', salesRoutes);
+router.use('/management', managementRoutes);
+
+
+
+export default router;
